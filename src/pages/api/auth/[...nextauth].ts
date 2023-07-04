7107116e4032e60e3de8b2c36ce0975f9aa8ff1f
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-07-04 01:47:18 -0300

    Atualizando o projeto

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
 import Auth0Provider from "next-auth/providers/auth0"
 
 export const authOptions: NextAuthOptions = {
