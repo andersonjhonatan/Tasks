9c7db94d8df66b39b04f2e3bba4b0beb66051ae1
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-07-04 14:30:02 -0300

    Atualizando o projeto

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,9 @@
-import Header from '@/src/components/Header/Header';
 import Dashboard from '@/src/components/Dashoboard/Dashboard';
 import React from 'react';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { getSession } from 'next-auth/react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]';
-/* export interface HomeProps {
-  user: {
-    name: string;
-  }
-} */
+
 export default function ShortHand() {
   return (
     <>
